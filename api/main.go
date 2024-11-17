c938cbd996ee83076dc47e824619c9a389aa80ca
jvc22 <joaovictorcostadasilva@outlook.com>
2024-11-17 16:24:27 -0300

    config: set CORS policy

--- api/main.go
+++ api/main.go
@@ -6,6 +6,22 @@ import (
 	"notask-app/routes"
 )
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	db, err := database.StartDatabase()
 	if err != nil {
@@ -17,7 +33,9 @@ func main() {
 
 	routes.SetupRoutes(mux, db)
 
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	handlerWithCORS := corsMiddleware(mux)
+
+	if err := http.ListenAndServe(":8080", handlerWithCORS); err != nil {
 		panic(err)
 	}
 }
