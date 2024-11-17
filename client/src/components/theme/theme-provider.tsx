089924d97a722acea7a96bcb03cd8fd85dc91d50
jvc22 <joaovictorcostadasilva@outlook.com>
2024-11-17 15:27:02 -0300

    fix: downgrade next-themes

--- client/src/components/theme/theme-provider.tsx
+++ client/src/components/theme/theme-provider.tsx
@@ -1,11 +1,16 @@
 'use client'
 
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import dynamic from 'next/dynamic'
+import { ThemeProviderProps } from 'next-themes/dist/types'
 import * as React from 'react'
 
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
+const NextThemesProvider = dynamic(
+  () => import('next-themes').then((e) => e.ThemeProvider),
+  {
+    ssr: false,
+  },
+)
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
